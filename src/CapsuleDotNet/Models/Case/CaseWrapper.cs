dd91020e6fa20ae1e85f9aa8b9585e73a12f1aec
jameseastham <j.eastham1993@gmail.com>
2019-07-28 09:08:09 +0100

    [FEATURE] Added case wrapper

---
+++ src/CapsuleDotNet/Models/Case/CaseWrapper.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CapsuleDotNet.Common;
+using Newtonsoft.Json;
+
+namespace CapsuleDotNet.Models{
+    public class CaseWrapper : DefaultObjectWrapper
+    {
+        private CaseWrapper(){}
+
+        private CaseWrapper(Case caseObject){
+            this.Case = caseObject;
+            this.Kases = null;
+        }
+
+        public static CaseWrapper Load(Case caseObject){
+            if (caseObject == null){
+                throw new ArgumentException("Case cannot be null");
+            }
+
+            return new CaseWrapper(caseObject);
+        }
+
+        [JsonProperty("kases")]
+        public IEnumerable<Case> Kases {get; set;}
+
+        [JsonProperty("kase")]
+        public Case Case {get; set;}
+    }
+}
